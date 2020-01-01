d0bb0222337f1c0b37fb821f2bc40c7d700f0e94
danwalterlink <danwalterlink@gmail.com>
2020-01-01 11:42:42 +0100

    distributive objects as destructuring

--- jsPatterns/distributiveObject.js
+++ jsPatterns/distributiveObject.js
@@ -1,11 +1,22 @@
 import { join } from "path";
 
+/**
+ * js does not implement pattern matching
+ * but the gist of pattern matching is just a branching  tree
+ * that could be emulated using conditionals
+ * but can also be approximated by string-matching names of sub-object-names
+ * 
+ * otherwise, the intelligent use of de- and restructuring assignments could lead to versions of pattern matching,
+ * whereby the 'topos' (topological shape) of an object is known
+ * and the correct structure loosely inferred
+ */
+
 let joiner = {
   root,
   kit: join(root, 'kit'),
   smoe: join(root, 'smoe'),
   jon: join(root, 'jon'),
-  get(fn) {
 
+  get(fn) {
   }
 }
